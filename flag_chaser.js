e5ba2a6f2f8cafe550d4707c4e3328f05aadbcd8
gitgud <xdevzone@live.fr>
2018-06-02 00:54:58 +0200

    log carrier name but not round

--- flag_chaser.js
+++ flag_chaser.js
@@ -23,13 +23,15 @@
 
         if ( team === 1 ) {
             
-            var carriername = $( "#blueflag-name" ).text();
+            // var carriername = $( "#blueflag-name" ).text();
+            var carriername = $( "#blueflag-name" )[0].childNodes[0].nodeValue;
             console.log("flag blue" + carriername);
             
         } else if ( team === 2 ) {
             
-            var carriername = $( "#redflag-name" ).text();
+            // var carriername = $( "#redflag-name" ).text();
             //var carrierid = 
+            var carriername = $( "#redflag-name" )[0].childNodes[0].nodeValue;
             console.log("flag red " + carriername);
             
         }
