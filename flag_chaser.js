8130e324cfbe8fd3625785c3f4c7e351d41aff94
gitgud <xdevzone@live.fr>
2018-06-02 20:42:57 +0200

    test GUI

--- flag_chaser.js
+++ flag_chaser.js
@@ -48,31 +48,7 @@
         });
     });
     
-    $("body").append("<div id='btnchaseblueflag' onClick='flagchase(1);' style='display: block; position: absolute;
-    left: 50%;
-    margin: -75px;
-    bottom: 300px;
-    width: 150px;
-    height: 25px;
-    padding: 5px;
-    background: rgba(0,0,0,0.5);
-    border-radius: 5px;
-    text-align: center;
-    color: #b4b4b4;
-    font-size: 15px;
-    cursor: pointer;'>Chase blue flag</div><div id='btnchaseredflag' onClick='flagchase(2);' style='display: block; position: absolute;
-    left: 50%;
-    margin: -75px;
-    bottom: 250px;
-    width: 150px;
-    height: 25px;
-    padding: 5px;
-    background: rgba(0,0,0,0.5);
-    border-radius: 5px;
-    text-align: center;
-    color: #b4b4b4;
-    font-size: 15px;
-    cursor: pointer;'>Chase red flag</div>");
+    $("body").append("<div id='btnchaseblueflag' onClick='flagchase(1);' style='display: block; position: absolute;left: 50%;margin: -75px;bottom: 300px;width: 150px;height: 25px;padding: 5px;background: rgba(0,0,0,0.5);border-radius: 5px;text-align: center;color: #b4b4b4;font-size: 15px;cursor: pointer;'>Chase blue flag</div><div id='btnchaseredflag' onClick='flagchase(2);' style='display: block; position: absolute;left: 50%;margin: -75px;bottom: 250px;width: 150px;height: 25px;padding: 5px;background: rgba(0,0,0,0.5);border-radius: 5px;text-align: center;color: #b4b4b4;font-size: 15px;cursor: pointer;'>Chase red flag</div>");
     
     function onKeydown ( event ) {
 
