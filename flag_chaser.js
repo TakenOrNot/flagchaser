8d52a9b7ff5a0d950188b66d8215017df7703232
gitgud <xdevzone@live.fr>
2018-06-02 05:19:53 +0200

    test

--- flag_chaser.js
+++ flag_chaser.js
@@ -10,9 +10,9 @@
     }
 
     function initEvents () {
-
+        SWAM.on ( 'keydown', onKeydown );
         SWAM.on ( 'CTF_FlagEvent', onFlagEvent );
-
+        
     }
 
     SWAM.on ( 'gameLoaded', init );
@@ -27,37 +27,65 @@
 
     };
     
-    /* EVENTS */
     var chaseflag = 0;
+    
+    /* EVENTS */
+    
+    function onKeydown ( event ) {
+
+        if ( event.originalEvent.key === 'o' ) { //TODO: This should be customizable
+
+          event.stopImmediatePropagation ();
+          console.log("chase blue flag");  
+          var chaseflag = 1;
+
+        }
+        
+        if ( event.originalEvent.key === 'p' ) { //TODO: This should be customizable
+
+          event.stopImmediatePropagation ();
+          console.log("chase red flag");  
+          var chaseflag = 2;
+
+        }
+
+    }
     
     function onFlagEvent ( event, team, verb ) {
         
-        if ( team === 1 && chaseflag === 1) {
+        // first, check if we are already spectating
+        
+        if( $('#btnFreeSpectator').css('display') == 'block' ) {
             
-            // var carriername = $( "#blueflag-name" ).text();
-            var carriername = $( "#blueflag-name" ).justtext();
-            console.log("flag blue " + carriername);
-            //$( "#scorecontainer:contains('carriername')" ).parent().parent().parent().data('player-id'); 
-            //$( "#scorecontainer:contains('carriername')" ).closest('.item').data('player-id');
-            if (carriername.length > 0){
-                carrierobj = Players.getByName(carriername); 
-                carrierid = carrierobj['id']; 
-                Network.sendCommand("spectate", carrierid + "");
+            console.log("chaseflag =" + chaseflag);
+            if ( team === 1 && chaseflag === 1) {
+
+                // var carriername = $( "#blueflag-name" ).text();
+                var carriername = $( "#blueflag-name" ).justtext();
+                console.log("flag blue " + carriername);
+                //$( "#scorecontainer:contains('carriername')" ).parent().parent().parent().data('player-id'); 
+                //$( "#scorecontainer:contains('carriername')" ).closest('.item').data('player-id');
+                if (carriername.length > 0){
+                    carrierobj = Players.getByName(carriername); 
+                    carrierid = carrierobj['id']; 
+                    Network.sendCommand("spectate", carrierid + "");
+                }
+            } else if ( team === 2 && chaseflag === 2) {
+
+                // var carriername = $( "#redflag-name" ).text();
+                //var carrierid = 
+                var carriername = $( "#redflag-name" ).justtext();
+                console.log("flag red " + carriername);
+                if (carriername.length > 0){
+                    //Network.sendCommand("/Spectate " + carriername);
+                    carrierobj = Players.getByName(carriername); 
+                    carrierid = carrierobj['id']; 
+                    Network.sendCommand("spectate", carrierid + "");
+                }
+
             }
-        } else if ( team === 2 && chaseflag === 2) {
-            
-            // var carriername = $( "#redflag-name" ).text();
-            //var carrierid = 
-            var carriername = $( "#redflag-name" ).justtext();
-            console.log("flag red " + carriername);
-            if (carriername.length > 0){
-                //Network.sendCommand("/Spectate " + carriername);
-                carrierobj = Players.getByName(carriername); 
-                carrierid = carrierobj['id']; 
-                Network.sendCommand("spectate", carrierid + "");
-            }
-            
-        }
+        } 
+        
 
         
       }
